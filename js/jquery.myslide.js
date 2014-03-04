be8f8a06e8a8284fde2dea6096313719cd6ccf41
Administrator <Administrator@2260f8b6-141f-8846-bc99-8ebd5fa9bd27>
2014-03-04 03:21:58 +0000

    对面从$("banner>a")变成$(".banner")
    git-svn-id: https://jamesGold:8443/svn/myslide@9 2260f8b6-141f-8846-bc99-8ebd5fa9bd27

--- js/jquery.myslide.js
+++ js/jquery.myslide.js
@@ -8,41 +8,65 @@ jQuery.fn.extend({
             haveNum: false,          //是否有数字导航
             linkText: false,         //是否有带透明的文字背景
             effect: "fade",          //效果
-            hoverChange:true,        //鼠标经过是否切换
+            hoverChange:false,        //鼠标经过是否切换
             hoverChagneClass:"on",    //鼠标移上去的样式
             delayTime:300,           //鼠标经过时对象切换迟滞时间，推荐值为300ms
             slideTime:1000
         }, options || {});
        
-        var n = -1;                                                       //索引
-        var achange = setInterval(autoChange, options.changeTime);        //定时器
-        var num = $(this).length - 1;                                     //计算幻灯片数量
-        var _self = $(this);
+        var n = -1;                                                   //索引
+        var _next=-1;;
+        var achange = setInterval(autoChange, options.changeTime);    //定时器
+        var max = $(this).find(".item").length - 1;      //计算幻灯片数量                  
+        var parent=$(this);
+        var _self = $(this).find(".item");
+        var _moveVal;                                                 //移动值
         //设置a的高度
         _self.css({
             height:_self.children("div").height()+"px",
             display:"block",
-            width:_self.children("div").width()+"px",
-            position:"absolute",
-            left:"0px",
-            top:"0px"
+            width:"100%"
         });
         var delayTime;//鼠标经过时对象切换迟滞时间，推荐值为300ms
         
         //效果
         function fadeAB() {
             switch(options.effect){
-                case "slide":  //向上翻
-                _self.slideUp(options.slideTime).parent().children().eq(n).slideDown(options.slideTime); 
+                //水平滚动
+                case "x":
+                _moveVal=_self.width()*_next;
+                if(_next==0&&_now==max){
+                  _self.eq(0).css({"left":_self.width()*(max+1)});
+                  _moveVal=_self.width()*(max+1);
+                }else if(_now==0){
+                   _self.eq(0).css({"left":""});
+                   parent.scrollLeft(0);
+                };
+                parent.animate({"scrollLeft":(_moveVal)});
                 break;
+                
+                //垂直滚动
+                case "y":
+                _moveVal=_self.height()*_next;
+                if(_next==0&&_now==max){
+                  _self.eq(0).css({"left":_self.height()*(max+1)});
+                  _moveVal=_self.height()*(max+1);
+                }else if(_now==0){
+                   _self.eq(0).css({"top":""});
+                   parent.scrollTop(0);
+                };
+                parent.animate({"scrollTop":(_moveVal)});
+                break;
+                
+                //淡入淡出
                 default:       //默认fade
-                _self.fadeOut(options.slideTime).parent().children().eq(n).fadeIn(options.slideTime); 
+                _self.fadeOut(options.slideTime).parent().children(".item").eq(_next).fadeIn(options.slideTime); 
                 break;
             }
         }
 
         if (options.linkText) {
-            _self.eq(num).after("<div class=\"sli-info\"></div>");
+            _self.eq(max).after("<div class=\"sli-info\"></div>");
             _self.parent().children(".sli-info").css({
                 position: "absolute",
                 width: "100%",
@@ -54,7 +78,7 @@ jQuery.fn.extend({
                 opacity: "0.7",
                 filter: "alpha(opacity=70)"
             });
-            _self.eq(num).after("<div class=\"sli-text \"></div>");
+            _self.eq(max).after("<div class=\"sli-text \"></div>");
             _self.parent().children(".sli-text").css({
                 position:"absolute",
                 bottom: "0px",
@@ -66,9 +90,9 @@ jQuery.fn.extend({
             function lkText(){
                 //文字
                 if(_self.attr("alt")){
-                   _self.parent().children(".sli-text").html(_self.eq(n).find("img").attr("alt"));
+                   _self.parent().children(".sli-text").html(_self.eq(_next).find("img").attr("alt"));
                 }else if(_self.attr("title").length){
-                   _self.parent().children(".sli-text").html(_self.eq(n).attr("title"));
+                   _self.parent().children(".sli-text").html(_self.eq(_next).attr("title"));
                 }
             }
 
@@ -82,11 +106,11 @@ jQuery.fn.extend({
         if (options.haveNum) {
             if(_self.parent().children(".slide_ul").length==0){
                 var slide_ul = "<ul class='slide_ul'>";
-                for (var i = 0; i <= num; i++) {
+                for (var i = 0; i <= max; i++) {
                     slide_ul += "<li >"+(i+1)+"</li>"
                 }
                 slide_ul += "</ul>";
-                _self.eq(num).after(slide_ul);
+                _self.eq(max).after(slide_ul);
             }
             _self.parent().children(".slide_ul").css({
                 "position": "absolute",
@@ -95,7 +119,7 @@ jQuery.fn.extend({
 
             _self.parent().children(".slide_ul").children("li").click(function() {
                 init_auto();
-                n=$(this).index();
+                _next=$(this).index();
                 fadeAB();
                 $(this).addClass(options.hoverChagneClass);
                 $(this).siblings().removeClass(options.hoverChagneClass);
@@ -106,13 +130,13 @@ jQuery.fn.extend({
             
             if(options.hoverChange){
                 _self.parent().children(".slide_ul").children("li").hover(function(){
-                init_auto();
-                n=$(this).index();
-                delayRun = setTimeout(fadeAB,options.delayTime);
-                $(this).addClass(options.hoverChagneClass);
-                $(this).siblings().removeClass(options.hoverChagneClass);
+                    init_auto();
+                    n=$(this).index();
+                    delayRun = setTimeout(fadeAB,options.delayTime);
+                    $(this).addClass(options.hoverChagneClass);
+                    $(this).siblings().removeClass(options.hoverChagneClass);
                     if (options.linkText) {
-                        
+                        lkText();
                     }
                 },function(){
                       clearTimeout(delayRun);
@@ -137,26 +161,30 @@ jQuery.fn.extend({
 
         //自动切换函数
         function autoChange() {
-            n = n >= num ? 0 : ++n;
+            _now=n;
+            n=_next;
+            _next= n>=max ? 0: ++n;
             if (options.haveNum) {
-                _self.parent().children(".slide_ul").children("li").eq(n).click();
+                _self.parent().children(".slide_ul").children("li").eq(_next).click();
             } else {
                 fadeAB();
-                init_auto();
+
+            }
+            if (options.linkText) {
+                    lkText();
             }
         }
 
         //上一个
         if (options.prevBtn) {
             $(options.prevBtn).click(function() {
-                n = n - 1 < 0 ? num :n-1;
-                if (options.haveNum) 
-                {
-                    _self.parent().children(".slide_ul").children("li").eq(n).click();
+                n=_next;
+                _next = n - 1 < 0 ? max :n-1;
+                if (options.haveNum){
+                    _self.parent().children(".slide_ul").children("li").eq(_next).click();
                     init_auto();
                 }
-                else
-                {
+                else{
                     fadeAB();  
                     init_auto();
                 } 
@@ -166,14 +194,12 @@ jQuery.fn.extend({
         //下一个
         if (options.nextBtn) {
             $(options.nextBtn).click(function(){
-                n = n + 1 > num ? 0 : ++n;
-                if (options.haveNum) 
-                {
-                    _self.parent().children(".slide_ul").children("li").eq(n).click();
+                n=_next;
+                _next= n>=max ? 0: ++n;
+                if (options.haveNum){
+                    _self.parent().children(".slide_ul").children("li").eq(_next).click();
                     init_auto();
-                }
-                else
-                {
+                }else{
                     fadeAB();  
                     init_auto();
                 } 
